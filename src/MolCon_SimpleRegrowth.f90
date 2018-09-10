86d7ee097ae8ecae2f7248ff4f2a1944a77e4bf7
tloeff <tloeff1@lsu.edu>
2018-09-10 00:37:47 -0500

    Laid the ground work for the mol constructor class

--- src/MolCon_SimpleRegrowth.f90
+++ src/MolCon_SimpleRegrowth.f90
@@ -2,31 +2,33 @@
 ! The purpose of this module is to provide the base class for the molecule
 ! constructor class.
 !==========================================================================================
-module Template_MolConstructor
+module MolCon_SimpleRegrowth
   use CoordinateTypes, only: Displacement, Perturbation
   use MasterTemplate, only: classyClass
   use VarPrecision
 
 
-  type, public, extends(classyClass) :: MolConstructor
+  type, public, extends(MolConstructor) :: SimpleRegrowth
     contains
-      procedure, public, pass :: Constructor
-      procedure, public, pass :: GenerateConfig
+      procedure, public, pass :: Constructor => SimpleRegrwoth_Constructor
+      procedure, public, pass :: GenerateConfig => SimpleRegrwoth_GenerateConfig
   end type
 !==========================================================================================
   contains
 !==========================================================================================
-  subroutine Constructor(self)
+  subroutine SimpleRegrowth_Constructor(self)
     implicit none
     class(MolConstructor), intent(inout) :: self
   end subroutine
 !==========================================================================================
-  subroutine GenerateConfig(self, trialBox, disp, probconstruct)
+  subroutine SimpleRegrowth_GenerateConfig(self, trialBox, disp, probconstruct)
     implicit none
     class(MolConstructor), intent(inout) :: self
     class(Perturbation), intent(inout) :: disp(:)
     class(SimpleBox), intent(inout) :: trialBox
     real(dp), intent(out) :: probconstruct = 1E0_dp
+
+
   end subroutine
 !==========================================================================================
 end module
